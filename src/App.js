2c497dbe881c9325c6682f9d4b041b0f2d78dc01
madhumitakhutia09 <113158551+arun28sarkar@users.noreply.github.com>
2024-09-20 11:25:02 +0530

    users page commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import { LoginComponent } from './components/LoginComponent';
+import { AddUser } from './components/AddUsers';
+import { ListUser } from './components/ListUsers';
+import { Sidebar } from './components/Sidebar';
+import { Dashboard } from './components/DashBoard';
 
 function App() {
-  
+  const location = useLocation();
+  const shouldShowSidebar = () => {
+    const { pathname } = location;
+    const check = ['/'].includes(pathname);
+    return !check;
+  };
   return (
-    <div></div>
+    <div>
+      {shouldShowSidebar() && <Sidebar />}
+      <Routes>
+        <Route exact path='/' element={<LoginComponent />} />
+        <Route exact path='/dashboard' element={<Dashboard />} />
+        <Route exact path='/userGroup/addUser' element={<AddUser />} />
+        <Route exact path='/userGroup/listUser' element={<ListUser />} />
+      </Routes>
+
+    </div>
   );
 }
 
