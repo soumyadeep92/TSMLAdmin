1fc0a89cc03c489128b5ef3e84d29f4fd5f6a418
madhumitakhutia09 <113158551+arun28sarkar@users.noreply.github.com>
2024-10-24 12:26:37 +0530

    main commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,130 +1,130 @@
-import React, { useState } from "react";
-import { Charts } from './Charts';
-import * as dayjs from "dayjs";
-import * as moment from 'moment';
+// import React from "react";
+// import { Dropdown, Container, Row, Col, Button, Card } from 'react-bootstrap';
+// import PieCharts from "./pieChart.js";
+// import AreaCharts from "./areaChart.js";
 
-export const Dashboard = () => {
-    const [seriesPie] = useState([44, 55, 13, 43, 22]);
-    const [optionsPie] = useState({
-        chart: {
-            width: 380,
-            type: 'pie',
-        },
-        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-        responsive: [
-            {
-                breakpoint: 480,
-                options: {
-                    chart: {
-                        width: 200,
-                    },
-                    legend: {
-                        position: 'bottom',
-                    },
-                },
-            },
-        ],
-    });
-    const [seriesArea] = useState([{
-        name: "Desktops",
-        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-    }]);
-    const [optionsArea] = useState({
-        chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-                enabled: false
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'straight'
-        },
-        title: {
-            text: 'Product Trends by Month',
-            align: 'left'
-        },
-        grid: {
-            row: {
-                colors: ['#f3f3f3', 'transparent'],
-                opacity: 0.5
-            },
-        },
-        xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-        }
-    });
-    const [seriesBar] = useState([{
-        name: "sales",
-        data: [{
-            x: '2019/01/01',
-            y: 400
-        }, {
-            x: '2019/04/01',
-            y: 430
-        }, {
-            x: '2019/07/01',
-            y: 448
-        }, {
-            x: '2019/10/01',
-            y: 470
-        }, {
-            x: '2020/01/01',
-            y: 540
-        }, {
-            x: '2020/04/01',
-            y: 580
-        }, {
-            x: '2020/07/01',
-            y: 690
-        }, {
-            x: '2020/10/01',
-            y: 690
-        }]
-    }]);
-    const [optionsBar] = useState({
-        chart: {
-            type: 'bar',
-            height: 380
-        },
-        xaxis: {
-            type: 'y',
-            labels: {
-                formatter: function (val) {
-                    return "Q" + moment(val).quarter()
-                }
-            },
-            group: {
-                style: {
-                    fontSize: '10px',
-                    fontWeight: 700
-                },
-                groups: [
-                    { title: '2019', cols: 4 },
-                    { title: '2020', cols: 4 }
-                ]
-            }
-        },
-        title: {
-            text: 'Grouped Labels on the X-axis',
-        },
-        tooltip: {
-            x: {
-                formatter: function (val) {
-                    return "Q" + moment(val).quarter() + " " + moment(val).format("YYYY")
-                }
-            }
-        },
-    },
-    );
-    return (
-        <>
-            <Charts options={optionsPie} type="pie" series={seriesPie} />
-            <Charts options={optionsArea} type="line" series={seriesArea} />
-            <Charts options={optionsBar} type="bar" series={seriesBar} />
-        </>
-    );
-}
+// export const Dashboard = () => {
+//     const points = [
+//         { x: 0, y: 100 },
+//         { x: 20, y: 20 },
+//         { x: 40, y: 50 },
+//         { x: 60, y: 20 },
+//         { x: 80, y: 50 },
+//         { x: 100, y: 20 }
+//       ];
+//     return (
+//         <>
+//             <Container fluid="true">
+//                 <Row style={rowStyle}>
+//                     <Col>
+//                         <Card style={cardStyle}>
+//                             <div style={shape}>
+//                                 <Card.Img style={images} src="/images/hourglass-end.png" />
+//                             </div>
+//                             <div style={shapeSide}>
+//                                 <p>200</p>
+//                                 <span>Open Pints</span>
+//                                 <p>200 Last Month</p>
+//                             </div>
+//                         </Card>
+//                     </Col>
+//                     <Col>
+//                         <Card style={cardStyle}>
+//                             <div style={shape}>
+//                                 <Card.Img style={images} src="/images/assept-document.png" />
+//                             </div>
+//                             <div style={shapeSide}>
+//                                 <p>200</p>
+//                                 <span>Open Pints</span>
+//                                 <p>200 Last Month</p>
+//                             </div>
+//                         </Card>
+//                     </Col>
+//                     <Col>
+//                         <Card style={cardStyle}>
+//                             <div style={shape}>
+//                                 <Card.Img style={images} src="/images/file.png" />
+//                             </div>
+//                             <div style={shapeSide}>
+//                                 <p>200</p>
+//                                 <span>Open Pints</span>
+//                                 <p>200 Last Month</p>
+//                             </div>
+//                         </Card>
+//                     </Col>
+//                     <Col>
+//                         <Card style={cardStyle}>
+//                             <div style={shape}>
+//                                 <Card.Img style={images} src="/images/meeting.png" />
+//                             </div>
+//                             <div style={shapeSide}>
+//                                 <p>200</p>
+//                                 <span>Open Pints</span>
+//                                 <p>200 Last Month</p>
+//                             </div>
+//                         </Card>
+//                     </Col>
+//                 </Row>
+//                 <Row style={rowStyle}>
+//                     <Col sm={5}>
+//                         <Card >
+//                             <Row style={{margin:'10px'}}>
+//                                 <Col><span style={{fontWeight:'bold',fontSize:'24px'}}>CAM Wise CVR</span></Col>
+//                                 <Col sm={5}>
+//                                     <Dropdown data-bs-theme="dark">
+//                                         <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+//                                             Dropdown Button
+//                                         </Dropdown.Toggle>
+
+//                                         <Dropdown.Menu>
+//                                             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+//                                             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+//                                             <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+//                                             <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+//                                         </Dropdown.Menu>
+
+//                                     </Dropdown>
+//                                 </Col>
+//                             </Row>
+//                             <div>
+//                                 <PieCharts />
+//                             </div>
+//                         </Card>
+//                     </Col>
+//                     <Col sm={7}>
+//                         <Card style={cardStyle}>
+//                             <Row style={{margin:'10px'}}>
+//                             <AreaCharts />
+//                             </Row>
+//                         </Card>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </>
+//     );
+// }
+// const rowStyle = {
+//     width: '87%',
+//     marginLeft: '135px',
+//     marginTop: '40px'
+// }
+// const cardStyle = {
+//     flexDirection: 'row'
+// }
+// const shapeSide = {
+//     width: '70%',
+//     marginTop: '15px'
+// }
+// const images = {
+//     display: 'block',
+//     height: '53px',
+//     width: '81%',
+//     margin: '8px'
+// }
+// const shape = {
+//     width: '90px',
+//     height: '75px',
+//     margin: '19px',
+//     borderRadius: '46%',
+//     backgroundColor: '#E5EBFF'
+// }
